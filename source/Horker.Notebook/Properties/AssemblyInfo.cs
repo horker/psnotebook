85bd9d52df16550da6e90a648b3f8090fa39a83c
horker <horker@users.noreply.github.com>
2019-08-15 20:05:37 +0900

    Update AssemblyInfo

--- source/Horker.Notebook/Properties/AssemblyInfo.cs
+++ source/Horker.Notebook/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
-[assembly: AssemblyTitle("PSNotebook")]
+[assembly: AssemblyTitle("Horker.Notebook")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PSNotebook")]
-[assembly: AssemblyCopyright("Copyright c  2019")]
+[assembly: AssemblyCompany("horker")]
+[assembly: AssemblyProduct("Horker.Notebook")]
+[assembly: AssemblyCopyright("Copyright (c) horker 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
-// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
-// その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
 
-// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
 [assembly: Guid("4fcd69f5-a3d4-4170-92fb-b88798daea4d")]
 
-// アセンブリのバージョン情報は次の 4 つの値で構成されています:
-//
-//      メジャー バージョン
-//      マイナー バージョン
-//      ビルド番号
-//      Revision
-//
-// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
-// 以下のように '*' を使用します:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
