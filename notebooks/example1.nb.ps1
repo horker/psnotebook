570b2bf0f940dabd73de5e2205304bdb1f950414
horker <horker@users.noreply.github.com>
2019-10-30 02:12:45 +0900

    Update images in README.md

---
+++ notebooks/example1.nb.ps1
@@ -0,0 +1,22 @@
+﻿#!Notebook v1
+
+#!CommandLine
+# Note: This session uses the ImageMagick tool set.
+#!CommandLine
+cd $env:TEMP
+#!CommandLine
+$wallpapers = dir -file C:\Windows\web\Wallpaper\Theme2
+$wallpapers.Name
+#!CommandLine
+$wallpapers | foreach { New-WpfImage $_.FullName } | New-WpfGrid
+#!CommandLine
+$wallpapers | foreach {
+    $newFile = $_ -replace ".jpg$", "_gray.jpg"
+    "$_ => $newFile"
+    convert -type grayscale $_.fullname $newFile
+}
+#!CommandLine
+dir *_gray.jpg | foreach { New-WpfImage $_.FullName } | New-WpfGrid
+#!CommandLine
+rm *_gray.jpg
+#!CommandLine
---
+++ notebooks/iris_analysis.nb.ps1
@@ -0,0 +1,11 @@
+﻿#!Notebook v1
+
+#!CommandLine
+cd $home\work\oxyplotcli
+#!CommandLine
+$iris = import-csv .\datasets\r\iris.csv
+#!CommandLine
+$iris | select -f 5 | ft
+#!CommandLine
+$iris | oxy.scat -xname Petal.Length -yname Petal.Width -groupname Species -AsPlotView
+#!CommandLine
